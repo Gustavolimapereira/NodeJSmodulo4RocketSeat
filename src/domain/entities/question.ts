176f841239ec494b4c0a05f26f1d6b07a5e6466e
gustavolimapereira <gu.lim@hotmail.com>
2024-09-03 23:21:43 -0300

    update

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
     title: string
     content:string
     slug:Slug
-    authorId:string,
+    authorId:UniqueEntityID,
+    bestAnswerId?: UniqueEntityID
+    createdAt: Date
+    updatedAr?: Date
 }
 
 class Questions extends Entity<QuestionProps> {
